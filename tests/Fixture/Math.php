45ae56e6139804ab5c042a3c0872b24cf8601da3
DKravtsov <dmitry.kravtsov@systemsdk.com>
2026-08-30 00:35:27 +0300

    testing SARIF report

--- tests/Fixture/Math.php
+++ tests/Fixture/Math.php
@@ -72,47 +72,6 @@ class PhpUnderControl_Example_Math
         return ($v1 - $v2);
     }
 
-    /**
-     * Not tested method that should be visible with low coverage.
-     * Note: Added check for skipping php attributes.
-     */
-    #[ORM\Entity]
-    #[ORM\Table(name: 'api_key')]
-    #[Groups([
-        'ApiKey',
-        'ApiKey.id',
-        'LogRequest.apiKey',
-    ])]
-    #[OA\Response(
-        response: 401,
-        description: 'Invalid token (not found or expired)',
-        content: new JsonContent(
-            properties: [
-                new Property(property: 'code', description: 'Error code', type: 'integer'),
-                new Property(property: 'message', description: 'Error description', type: 'string'),
-            ],
-            type: 'object',
-            example: [
-                'code' => 401,
-                'message' => 'JWT Token not found',
-            ],
-        ),
-    )]
-    #[OA\Response(
-        response: 403,
-        description: 'Access denied',
-        content: new JsonContent(
-            properties: [
-                new Property(property: 'code', description: 'Error code', type: 'integer'),
-                new Property(property: 'message', description: 'Error description', type: 'string'),
-            ],
-            type: 'object',
-            example: [
-                'code' => 403,
-                'message' => 'Access denied',
-            ],
-        ),
-    )]
     public function div($v1, $v2)
     {
         $v3 = $v1 / ($v2 + $v1);
@@ -174,47 +133,7 @@ class PhpUnderControl_Example_Math
         return $v8;
     }
 
-    /**
-     * Simple copy for cpd detection.
-     */
-    #[ORM\Entity]
-    #[ORM\Table(name: 'api_key')]
-    #[Groups([
-        'ApiKey',
-        'ApiKey.id',
-        'LogRequest.apiKey',
-    ])]
-    #[OA\Response(
-        response: 401,
-        description: 'Invalid token (not found or expired)',
-        content: new JsonContent(
-            properties: [
-                new Property(property: 'code', description: 'Error code', type: 'integer'),
-                new Property(property: 'message', description: 'Error description', type: 'string'),
-            ],
-            type: 'object',
-            example: [
-                'code' => 401,
-                'message' => 'JWT Token not found',
-            ],
-        ),
-    )]
-    #[OA\Response(
-        response: 403,
-        description: 'Access denied',
-        content: new JsonContent(
-            properties: [
-                new Property(property: 'code', description: 'Error code', type: 'integer'),
-                new Property(property: 'message', description: 'Error description', type: 'string'),
-            ],
-            type: 'object',
-            example: [
-                'code' => 403,
-                'message' => 'Access denied',
-            ],
-        ),
-    )]
-    public function complex($v1, $v2)
+    public function div($v1, $v2)
     {
         $v3 = $v1 / ($v2 + $v1);
         if ($v3 > 14)
